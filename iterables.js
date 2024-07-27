5753dcc4be4253f07bbe2168a798a2b05192ad3b
Keshav chand <Keshavchand204@gmail.com>
2024-07-27 23:25:32 +0530

    first commit

---
+++ iterables.js
@@ -0,0 +1,17 @@
+const days=[
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday'
+];
+//for of
+for(let items of days){
+    console.log(items);
+}
+
+const greeting='Good Morning';
+for(let char of greeting){
+    console.log(char);
+}
